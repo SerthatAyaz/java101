07303f4273c3f3773360f9ddeebec651d5ca1cbf
SerthatAyaz <100021280+SerthatAyaz@users.noreply.github.com>
2022-07-23 17:36:02 +0300

    arrays

---
+++ src/Arrays/HarmonicAverage.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class HarmonicAverage {
+    public static void main(String[] args) {
+        int[] list = {4, 5, 6, 7, 8, 9};
+
+        double sum = 0;
+
+        for (double i : list) {
+            sum += 1 / i;
+        }
+
+        double average = list.length / sum;
+
+        System.out.println(average);
+    }
+}
